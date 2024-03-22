249f721e7ba51ae039c14f95e40b5d2025f2c168
Beyza Evcen <beyzaevcen37@icloud.com>
2024-03-22 20:44:24 +0300

    Changed redirect to popup

--- frontend/src/pages/loginsignup/LoginSignup.js
+++ frontend/src/pages/loginsignup/LoginSignup.js
@@ -1,31 +1,20 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Link bileşeni eklenmiştir.
-import './login.css';
 import { useAuth0 } from '@auth0/auth0-react';
 
 export const LoginSignup = () => {
-  const { loginWithPopup, logout, isAuthenticated } = useAuth0();
+  const { loginWithPopup, isAuthenticated } = useAuth0();
 
-  // Kullanıcı girişi durumu değiştiğinde otomatik yönlendirme yapılacak.
   useEffect(() => {
     if (isAuthenticated) {
-      window.location.href = '/films';
+      window.location.href = '/films'; 
+    } else {
+      loginWithPopup();
     }
-  }, [isAuthenticated]);
+  }, [isAuthenticated, loginWithPopup]);
 
   return (
-    <div className='login_main'>
-      <ul>
-        <li><button onClick={loginWithPopup}>Login with redirect</button></li>
-        <li><button onClick={logout}>Logout</button></li>
-      </ul>
-      <h3>User is {isAuthenticated ? "logged in " : "not logged in"}</h3>
-      {/* isAuthenticated true ise '/films' sayfasına, değilse '/login' sayfasına yönlendirir. */}
-      {isAuthenticated ? (
-        <Link to="/films">Go to Films</Link>
-      ) : (
-        <Link to="/">Go to Login</Link>
-      )}
+    <div>
+      <h1>Welcome to Film Archive</h1>
     </div>
-  );
+  ); 
 }
